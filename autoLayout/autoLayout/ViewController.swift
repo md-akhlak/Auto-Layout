1d77beada9770b0b62471913a5df85a987106559
mdakhlak <mohammadakhlak121@gmail.com>
2024-11-20 11:21:39 +0530

    Added: autoLayout

---
+++ autoLayout/autoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Batch - 1 on 20/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ autoLayout/autoLayoutTests/autoLayoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  autoLayoutTests.swift
+//  autoLayoutTests
+//
+//  Created by Batch - 1 on 20/11/24.
+//
+
+import Testing
+@testable import autoLayout
+
+struct autoLayoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
